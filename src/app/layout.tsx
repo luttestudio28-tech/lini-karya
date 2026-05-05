567221a98d33e503c55be14a2d84f63aa3834461
imanabdulm <luttestudio28@gmail.com>
2026-05-05 10:33:48 +0700

    Add Meta Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,28 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '933664496170103');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=933664496170103&ev=PageView&noscript=1"
+          />
+        </noscript>
         <CustomCursor />
         <Navbar />
         <main className="flex-grow">{children}</main>
